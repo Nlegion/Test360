1e63887cd852733f5f01884db00d430681726eff
nlegion <nparanoid@gmail.com>
2016-01-06 22:36:55 +0300

    system can publish tests
    допилена система до состоянии публикации теста

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,14 +1,24 @@
 # AnswersController
 class AnswersController < ApplicationController
-  before_action :load_test
-  
-  def index
-    @answers = Answer.where(test_id: @tes)
-  end
+  before_action :load_test, :load_questions
+
+  def get_answ
+  end 
+
+  def post_answ
+    @questions.each do |q|
+      Answer.create(name: params[q.id.to_s], question_id: q.id, test_id: @test.id) if params[q.id.to_s].present?
+    end
+    redirect_to test_path(@test)
+  end 
 
   private
 
   def load_test
     @test = Test.find(params[:test_id])
+  end
+
+  def load_questions
+    @questions = @test.questions.order("RANDOM()")
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   root 'tests#index' 
   resources :tests do
     resources :questions, except: [:index, :show]
-    resources :answers, only: [:index]
+    resources :answers do
+      get 'get_answ', on: :collection
+      post 'post_answ', on: :collection
+    end
   end
 end
